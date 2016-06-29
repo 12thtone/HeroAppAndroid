8ad17833f5326d6e3d8097ac388c569cf9f12b1f
12thtone <matt@gommmd.com>
2016-06-29 13:36:33 -0400

    Fragments tut

--- app/src/main/java/com/mmmd/maher/heroappandroid/Activities/MainActivity.java
+++ app/src/main/java/com/mmmd/maher/heroappandroid/Activities/MainActivity.java
@@ -1,11 +1,16 @@
 package com.mmmd.maher.heroappandroid.Activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
+import com.mmmd.maher.heroappandroid.Fragments.MainFragment;
+import com.mmmd.maher.heroappandroid.Fragments.PickPowerFragment;
 import com.mmmd.maher.heroappandroid.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainFragment.MainFragmentInteractionListener, PickPowerFragment.PickPowerInteractionListener {
 
 
 //
@@ -13,5 +18,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }//
+
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new MainFragment();
+            manager.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+
+    public void loadPickPowerScreen() {
+        PickPowerFragment pickPowerFragment = new PickPowerFragment();
+        this.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, pickPowerFragment).addToBackStack(null).commit();
+    }
+
+    @Override
+    public void onMainFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onPickPowerFragmentInteraction(Uri uri) {
+
+    }
 }
