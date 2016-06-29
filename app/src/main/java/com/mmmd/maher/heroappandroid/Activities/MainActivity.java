de0124370c658640b3c0b8a085f095775043cea2
maher <matt@gommd.com>
2016-06-29 11:03:38 -0400

    Inital

---
+++ app/src/main/java/com/mmmd/maher/heroappandroid/Activities/MainActivity.java
@@ -0,0 +1,17 @@
+package com.mmmd.maher.heroappandroid.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mmmd.maher.heroappandroid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+//
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
